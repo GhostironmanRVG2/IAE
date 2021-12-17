053ed3662d94fbb2ba58ba06b12b304a09d77dd2
MARTINS <tiagomartinas2000@gmail.com>
2021-12-17 21:27:34 +0000

    order_db

--- BACK-END/controllers/orders_controller.js
+++ BACK-END/controllers/orders_controller.js
@@ -1,7 +1,8 @@
 const connect = require('../config/connect');
 const purchase_order_controller = require('./purchase_order_controller');
+//FUNÇÃO PARA DEOLVER OS DADOS DOORDER E SUPPLIER 
 function read(req, res) {
-    connect.con.query('SELECT iae.order.order_id, iae.order.supplier_id, iae.order.date, iae.order.expire_date, iae.order.total, iae.order.document_id, iae.supplier.designation FROM iae.order join iae.supplier ON iae.order.supplier_id = iae.supplier.supplier_id;' , function(error, result){
+    connect.con.query('SELECT iae.order.order_id, iae.order.supplier_id, iae.order.date, iae.order.expire_date, iae.order.total, iae.order.document_id, iae.supplier.designation, count(iae.ordered_product.order_id) as OrderedProducts_orderid_count FROM iae.order join iae.supplier ON iae.order.supplier_id = iae.supplier.supplier_id JOIN iae.ordered_product ON iae.ordered_product.order_id = iae.order.order_id' , function(error, result){
       //caso de erro ,manda msg de erro
         if (error){
           //ENVIAR STATUS DE ERRO
@@ -18,13 +19,28 @@ function read(req, res) {
     });
 }
 
-/*
-function readID(req, res) {
+
+function OrderProductByID(req, res) {
     const order_id = req.sanitize('order_id').escape();
-    connect.con.query('SELECT iae.order.')
+    connect.con.query('SELECT iae.order.order_id as order_id, iae.order.supplier_id as supplier_id_order, iae.order.date as date_order, iae.order.expire_date as expire_date_order, iae.order.total as total_order, iae.order.document_id as document_id_order, iae.ordered_product.description as designation_ordered_product, iae.ordered_product.order_id as order_id_ordered_product, iae.ordered_product.ordered_product_id as ordered_product_id_ordered_product, iae.ordered_product.quantity as quantity_ordered_product, iae.ordered_product.total as total_ordered_product, iae.ordered_product.unit_price as unit_price_ordered_product FROM iae.ordered_product JOIN iae.order ON iae.ordered_product.order_id = iae.order.order_id WHERE iae.order.order_id = ?', order_id, function(err, rows, fields) {
+        if (!err) {
+            //verifica os resultados se o número de linhas for 0 devolve dados não encontrados, caso contrário envia os resultados (rows).
+            if (rows.length == 0) {
+                res.status(404).send({
+                    "msg": "data not found"
+                });
+            } else {
+                res.status(200).send(rows);
+            }
+        } else
+        res.status(400).send({
+            "msg": err.code
+        });
+    });
 }
-*/
+
 
 module.exports = {
-    read: read
+    read: read,
+    OrderProductByID: OrderProductByID
 }
--- BACK-END/routes/orders_route.js
+++ BACK-END/routes/orders_route.js
@@ -1,4 +1,5 @@
 const app=require('../server');
 const orders=require('../controllers/orders_controller');
 app.get('/OrdersGetAll/', orders.read);
+app.get('/OrderProductByOrderID/:order_id', orders.OrderProductByID);
 
