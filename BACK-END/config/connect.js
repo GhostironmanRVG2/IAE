60e5ddc89f7ed66ed4eb6a0f5f517ffca126eecd
Miguel Amorim <miguelitoamorimgeral@gmail.com>
2021-12-17 23:27:07 +0000

    lol

--- BACK-END/config/connect.js
+++ BACK-END/config/connect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 module.exports = {
 	con: mysql.createConnection({
 		host     : '2.83.78.5',
-		user     : 'martins',
+		user     : 'database',
 		password : 'Winx2021',
 		database : 'iae',
     port     : '3307'
